cb84941b102c3f3256dbf6edc3b68d471a523ad3
khanhdc2901 <duykdc2901@gmail.com>
2026-02-02 09:06:26 +0700

    fix: fix some UI

--- src/app/(main)/student/layout.jsx
+++ src/app/(main)/student/layout.jsx
@@ -3,12 +3,12 @@ import Sidebar from '@/shared/components/Sidebar';
 
 export default function Dashboards({ children }) {
   return (
-    <div className='flex h-screen'>
+    <div className='flex min-h-screen'>
       <Sidebar />
 
-      <div className='flex flex-col flex-1 overflow-hidden'>
+      <div className='flex flex-col flex-1 '>
         <Header />
-        <main className='flex-1 p-6 bg-gray-100 overflow-hidden'>{children}</main>
+        <main className='flex-1 p-6 bg-gray-100 '>{children}</main>
       </div>
     </div>
   );
